d95c52022d7c6ca481f6d52755bf61618340a3e4
Jeferson Barros <soldier.jhonson@gmail.com>
2017-05-27 12:24:55 -0400

    Add initial flame level1

--- js/states/level1_state.js
+++ js/states/level1_state.js
@@ -15,6 +15,7 @@
         this.game.load.tilemap('Level1','assets/maps/level1.json', null, Phaser.Tilemap.TILED_JSON);
         this.game.load.image('mapTiles', 'assets/spritesheets/tiled-fases.png');
         this.game.load.audio('environmentSound', 'assets/sounds/environment.ogg');
+
     }
 
     Level1State.prototype.create = function() {
@@ -34,18 +35,61 @@
         this.wallsLayer.resizeWorld();
         
         //Tile maps - collision
-        this.Level1.setCollisionByExclusion([8,9,19,11,16,17,18,19], true, this.wallsLayer);
+        this.Level1.setCollisionByExclusion([19,20,21,22,23,24,11,16,17,18,19], true, this.wallsLayer);
         this.Level1.setCollision([5,6,13], true, this.lavaLayer);
         
         // setup initial player properties
         this.player.setup(this);
-        this.player.sprite.x = 828;
+        this.player.sprite.x = 628;
         this.player.sprite.y = 70;
         
         //Movimentacao de camera
         this.game.camera.follow(this.player.sprite);
         
+        //Fire effect with phaser particles
+        var emitter;
+        var pSize = this.game.world.width / 22.5;
+        var bmpd = this.game.add.bitmapData(pSize, pSize);
+        // Create a radial gradient, yellow-ish on the inside, orange
+        // on the outside. Use it to draw a circle that will be used
+        // by the FireParticle class.
+        var grd = bmpd.ctx.createRadialGradient(
+        pSize / 2, pSize /2, 2,
+        pSize / 2, pSize / 2, pSize * 0.5);
+        grd.addColorStop(0, 'rgba(193, 170, 30, 0.6)');
+        grd.addColorStop(1, 'rgba(255, 100, 30, 0.1)');
+        bmpd.ctx.fillStyle = grd;
+        
+        bmpd.ctx.arc(pSize / 2, pSize / 2 , pSize / 2, 0, Math.PI * 2);
+        bmpd.ctx.fill();
+        
+        this.game.cache.addBitmapData('flame', bmpd);
+        
+        // Generate 100 particles
+        emitter = this.game.add.emitter(this.game.world.centerX, this.game.world.height, 100);
+        emitter.width = 3 * pSize;
+        emitter.particleClass = FireParticle;
+        
+        // Magic happens here, bleding the colors of each particle
+        // generates the bright light effect
+        emitter.blendMode = PIXI.blendModes.ADD;
+        emitter.makeParticles();
+        emitter.minParticleSpeed.set(-15, -80);
+        emitter.maxParticleSpeed.set(15, -100);
+        emitter.setRotation(0, 0);
+        // Make the flames taller than they are wide to simulate the
+        // effect of flame tongues
+        emitter.setScale(3, 1, 4, 3, 12000, Phaser.Easing.Quintic.Out);
+        emitter.gravity = -5;
+        emitter.start(false, 3000, 50);
     }
+    
+    function FireParticle(game, x, y) {
+        Phaser.Particle.call(this, game, x, y, game.cache.getBitmapData('flame'));
+    }
+    
+    FireParticle.prototype = Object.create(Phaser.Particle.prototype);
+    FireParticle.prototype.constructor = FireParticle;
     
     Level1State.prototype.update = function() {
         this.game.physics.arcade.collide(this.player.sprite, this.wallsLayer, this.player.groundCollision, null, this.player);
