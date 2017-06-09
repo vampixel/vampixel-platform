452285603c2a9720a56579a5bc3ca376924ff3fe
FernandoDantas <fernandoalbino755@gmail.com>
2017-06-09 11:42:53 -0400

    Instruction Screen

--- js/states/menu_state.js
+++ js/states/menu_state.js
@@ -7,8 +7,10 @@
     MenuState.prototype.preload = function() {
         this.onMenu = true;
         this.game.load.image('background', 'assets/img/menu.png');
-        this.game.load.image('start', 'assets/img/start.png');
+        this.game.load.image('startButton', 'assets/img/startButton.png');
         this.game.load.image('chooseLevelButton', 'assets/img/chooseLevelButton.png');
+        this.game.load.image('instructionButton', 'assets/img/start.png');
+        
         this.game.load.audio('menuSound', 'assets/sounds/gameSoundMenu.ogg');
         this.game.load.audio('clickSound', 'assets/sounds/click.ogg');
     }   
@@ -21,7 +23,7 @@
         this.game.add.tileSprite(0, 0, 800, 600, 'background');
         
         this.clickSound = this.game.add.audio('clickSound');
-        
+    
         this.menuSound = this.game.add.audio('menuSound');
         this.menuSound.loop = true;
         this.menuSound.play();
@@ -31,11 +33,14 @@
         //var text = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 150, 'Vampixel', { fill: '#ffffff', align: 'center', fontSize: 80 });
         //text.anchor.set(0.5);
 
-        var startButton = this.game.add.button(this.game.world.centerX, this.game.world.centerY + 150, 'start', startButtonClicked, this);
+        var startButton = this.game.add.button(this.game.world.centerX -230, this.game.world.centerY +150, 'startButton', startButtonClicked, this);
         startButton.anchor.set(0.5);
         
-        var chooseLevelButton = this.game.add.button(this.game.world.centerX, this.game.world.centerY + 230, 'chooseLevelButton', chooseLevelButton, this);
+        var chooseLevelButton = this.game.add.button(this.game.world.centerX, this.game.world.centerY +150, 'chooseLevelButton', chooseLevelButton, this);
         chooseLevelButton.anchor.set(0.5);
+        
+        var instructionButton = this.game.add.button(this.game.world.centerX + 230, this.game.world.centerY +150, 'instructionButton', instructionButton, this);
+        instructionButton.anchor.set(0.5);
 
         function startButtonClicked() {
             this.clickSound.play();
@@ -47,6 +52,12 @@
             this.clickSound.play();
             this.game.state.start('level'+ prompt("Digite a fase"));
             this.menuSound.stop();
+        }
+
+        function  instructionButton() {
+            this.clickSound.play();
+            this.menuSound.stop();
+            this.game.state.start('menuInstructions');
         }
     }
     
