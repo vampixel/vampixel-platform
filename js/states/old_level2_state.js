2ebbf49b10139af5038d663d863ba9247757946b
Jeferson Barros <soldier.jhonson@gmail.com>
2017-06-21 03:19:22 -0400

    Add platforms is down

--- js/states/old_level2_state.js
+++ js/states/old_level2_state.js
@@ -1,12 +1,12 @@
 (function () {
     'use strict'; 
 
-    var Level2State = function(Level2) {
+    var Level1State = function(Level1) {
         // load sprites here
         this.player = gameManager.getSprite('player'); 
     };
     
-    Level2State.prototype.preload = function() {
+    Level1State.prototype.preload = function() {
         // Carregando o Player
         this.player.preload();
         
@@ -15,23 +15,19 @@
         this.game.load.spritesheet('items', 'assets/spritesheets/items.png', 32, 32, 16);
         this.game.load.spritesheet('blood', 'assets/img/blood.png', 42, 42, 1);
         this.game.load.spritesheet('capa_hud', 'assets/spritesheets/capa_hud.png', 64, 64, 1);
-        this.game.load.spritesheet('fire', 'assets/spritesheets/sprite-fogo-chamine-32x32.png', 32, 32, 4);    
-
         
         // Images
         this.game.load.image('tiledFases', 'assets/spritesheets/tiled-fases.png');
-        this.game.load.image('stick', 'assets/img/estacas.png');
-        this.game.load.spritesheet('platform', 'assets/img/plataformas-que-caem-32x32.png', 32,32, 8);
         
         //Tile maps
-        this.game.load.tilemap('Level2','assets/maps/level_2_chamine.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.tilemap('Level1','assets/maps/level1.json', null, Phaser.Tilemap.TILED_JSON);
         
         // Sounds
-        this.game.load.audio('environmentSoundLevel2', 'assets/sounds/levels/simonMathewson8bitEnergyDrinkComedown1.ogg');
+        this.game.load.audio('environmentSoundLevel1', 'assets/sounds/levels/simonMathewson8bitEnergyDrinkComedown1.ogg');
     }
 
-    Level2State.prototype.create = function() {
-        
+    Level1State.prototype.create = function() {
+
         // set globals
         gameManager.globals.lives = 3;
 
@@ -39,26 +35,23 @@
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         
         // Áudios
-        gameManager.globals.environmentSoundLevel2 = this.game.add.audio('environmentSoundLevel2');
-        gameManager.globals.environmentSoundLevel2.lopp = true;
-        gameManager.globals.environmentSoundLevel2.play();
+        gameManager.globals.environmentSoundLevel1 = this.game.add.audio('environmentSoundLevel1');
+        gameManager.globals.environmentSoundLevel1.loop = true;
+        gameManager.globals.environmentSoundLevel1.play();
     
         //Tile maps
-        this.Level2 = this.game.add.tilemap('Level2');
-        this.Level2.addTilesetImage('tiled-fases','tiledFases');
-        //this.Level2.addTilesetImage('estacas','estacas');
-        //this.Level2.addTilesetImage('plataformas','plataformas');
-        //this.Level2.addTilesetImage('fogo','fogo');
+        this.Level1 = this.game.add.tilemap('Level1');
+        this.Level1.addTilesetImage('tiled-fases','tiledFases');
         
-        this.bgLayer = this.Level2.createLayer('Bg');
-        this.fireLayer = this.Level2.createLayer('Fire');
-        this.wallsLayer = this.Level2.createLayer('Walls');
+        this.bgLayer = this.Level1.createLayer('Bg');
+        this.fireLayer = this.Level1.createLayer('Fire');
+        this.wallsLayer = this.Level1.createLayer('Walls');
         this.wallsLayer.resizeWorld();
         
         //Tile maps - collision
-        //this.Level2.setCollisionByExclusion([19,20,21,22,23,24], true, this.wallsLayer);
-        this.Level2.setCollisionByExclusion([], true, this.wallsLayer);
-        this.Level2.setCollisionByExclusion([], true, this.fireLayer);
+        //this.Level1.setCollisionByExclusion([19,20,21,22,23,24], true, this.wallsLayer);
+        this.Level1.setCollisionByExclusion([], true, this.wallsLayer);
+        this.Level1.setCollisionByExclusion([], true, this.fireLayer);
         
         // setup initial player properties and camera follow
         this.player.setup(this);
@@ -67,58 +60,29 @@
         this.game.camera.follow(this.player.sprite);
         
         // texto do level
-        this.Level2Text = this.game.add.text(this.game.world.centerX + 180, 30, 'Level 2', { fill: '#ffffff', align: 'center', fontSize: 27 });
-        this.Level2Text.anchor.set(0.5);
-        this.Level2Text.fixedToCamera = true;  
-        
-        // Platforms group
-        this.platforms = this.game.add.physicsGroup();
-        this.Level2.createFromObjects('Platforms', 'platform', 'platform', 0, true, false, this.platforms);
-        // Para cada objeto do grupo, vamos executar uma função
-        this.platforms.forEach(function(platform){
-            platform.body.immovable = true;
-        });
-        
-        
-        // Sticks group
-        this.sticks = this.game.add.physicsGroup();
-        this.Level2.createFromObjects('Sticks', 'stick', 'stick', 0, true, false, this.sticks);
-        // Para cada objeto do grupo, vamos executar uma função
-        this.sticks.forEach(function(stick){
-            stick.body.immovable = true;
-        });
-        
-        // Grupo de chamas (fogo)
-        this.flames = this.game.add.physicsGroup();
-        this.Level2.createFromObjects('Flames', 'flame', 'fire', 0, true, false, this.flames);
-        // Para cada objeto do grupo, vamos executar uma função
-        this.flames.forEach(function(fire){
-            // body.immovable = true indica que o objeto não é afetado por forças externas
-            fire.body.immovable = true;
-            // Adicionando animações; o parâmetro true indica que a animação é em loop
-            fire.animations.add('fireAnim', [0,1,2,3,2,1],6, true);
-            fire.animations.play('fireAnim');
-        });
+        this.level1Text = this.game.add.text(this.game.world.centerX + 180, 30, 'Level 1', { fill: '#ffffff', align: 'center', fontSize: 27 });
+        this.level1Text.anchor.set(0.5);
+        this.level1Text.fixedToCamera = true;  
         
         // Life
         this.livesToCollect = this.game.add.physicsGroup();
-        this.Level2.createFromObjects('Items', 'life', 'blood', 0, true, false, this.livesToCollect);
-        this.livesToCollect.forEach(function(addlifeLevel2) {
-            addlifeLevel2.anchor.setTo(0.5);
-            addlifeLevel2.body.immovable = true;
+        this.Level1.createFromObjects('Items', 'life', 'blood', 0, true, false, this.livesToCollect);
+        this.livesToCollect.forEach(function(addlifeLevel1) {
+            addlifeLevel1.anchor.setTo(0.5);
+            addlifeLevel1.body.immovable = true;
         });
         
         // Capas Level 1
-        this.capasToCollectLevel2 = this.game.add.physicsGroup();
-        this.Level2.createFromObjects('Items', 'capa', 'capa_hud', 0, true, false, this.capasToCollectLevel2);
-        this.capasToCollectLevel2.forEach(function(addCapaLevel2) {
-            addCapaLevel2.anchor.setTo(0.5);
-            addCapaLevel2.body.immovable = true;
+        this.capasToCollectLevel1 = this.game.add.physicsGroup();
+        this.Level1.createFromObjects('Items', 'capa', 'capa_hud', 0, true, false, this.capasToCollectLevel1);
+        this.capasToCollectLevel1.forEach(function(addCapaLevel1) {
+            addCapaLevel1.anchor.setTo(0.5);
+            addCapaLevel1.body.immovable = true;
         });
         
         //Ratos
         this.ratos = this.game.add.physicsGroup();
-        this.Level2.createFromObjects('Enemies', 'rato', 'rato', 8, true, false, this.ratos);
+        this.Level1.createFromObjects('Enemies', 'rato', 'rato', 8, true, false, this.ratos);
         this.ratos.forEach(function(rato){
             rato.anchor.setTo(0, 0);
             rato.body.immovable = true;
@@ -130,7 +94,7 @@
         
         // Grupo de fireBullets
         this.fireBullets = this.game.add.physicsGroup();
-        this.Level2.createFromObjects('Enemies', 'fireBullet', 'items', 5, true, false, this.fireBullets);
+        this.Level1.createFromObjects('Enemies', 'fireBullet', 'items', 5, true, false, this.fireBullets);
         
         // Para cada objeto do grupo, vamos executar uma função
         this.fireBullets.forEach(function(fireBullet){
@@ -145,7 +109,7 @@
         
         // Grupo de diamantes
         this.diamonds = this.game.add.physicsGroup();
-        this.Level2.createFromObjects('Items', 'diamond', 'items', 5, true, false, this.diamonds);
+        this.Level1.createFromObjects('Items', 'diamond', 'items', 5, true, false, this.diamonds);
         // Para cada objeto do grupo, vamos executar uma função
         this.diamonds.forEach(function(diamond){
             // body.immovable = true indica que o objeto não é afetado por forças externas
@@ -158,7 +122,7 @@
         // Grupo de inimigos
         this.bats = this.game.add.physicsGroup();
         
-        this.Level2.createFromObjects('Enemies', 'bat', 'enemies', 8, true, false, this.bats);
+        this.Level1.createFromObjects('Enemies', 'bat', 'enemies', 8, true, false, this.bats);
         this.bats.forEach(function(bat){
             bat.anchor.setTo(0.5, 0.5);
             bat.body.immovable = true;
@@ -171,7 +135,6 @@
             bat.body.bounce.x = 1;
         });
         
-        /*
         //Fire effect with phaser particles
         var emitter;
         var pSize = this.game.world.width / 12.5;
@@ -209,7 +172,6 @@
         emitter.setScale(3, 1, 4, 3, 12000, Phaser.Easing.Quintic.Out);
         emitter.gravity = -20;
         emitter.start(false, 3000, 50);
-        */
     }
     
     function FireParticle(game, x, y) {
@@ -219,15 +181,13 @@
     FireParticle.prototype = Object.create(Phaser.Particle.prototype);
     FireParticle.prototype.constructor = FireParticle;
     
-    Level2State.prototype.update = function() {
+    Level1State.prototype.update = function() {
         if (gameManager.globals.qtdeCapas > 0){
             gameManager.globals.haveCapas = true;
         }
         
-        //Collisões
-        this.game.physics.arcade.overlap(this.player.sprite, this.platforms, this.platformsCollision, null, this);
         this.game.physics.arcade.collide(this.player.sprite, this.fireLayer, this.fireDeath, null, this);
-        this.game.physics.arcade.overlap(this.player.sprite, this.sticks, this.sticksCollision, null, this);
+    
         // Colisão do Player com os Diamantes e com os Inimigos "Ratos"
         this.game.physics.arcade.overlap(this.player.sprite, this.diamonds, this.diamondCollect, null, this); 
         this.game.physics.arcade.overlap(this.player.sprite, this.ratos, this.ratosCollision, null, this);
@@ -239,7 +199,7 @@
         this.game.physics.arcade.overlap(this.player.sprite, this.livesToCollect, this.player.livesToCollectCollision, null, this.player); 
         
         // Player pegando capa
-        this.game.physics.arcade.overlap(this.player.sprite, this.capasToCollectLevel2, this.player.capasToCollectCollision, null, this.player); 
+        this.game.physics.arcade.overlap(this.player.sprite, this.capasToCollectLevel1, this.player.capasToCollectCollision, null, this.player); 
         
         // Objetos com as paredes e Plataformas
         this.game.physics.arcade.collide(this.bats, this.wallsLayer);
@@ -251,7 +211,6 @@
         
         this.player.handleInputs();
         //console.log("Animation: ", this.player.sprite.animations.currentAnim.name);
-        //console.log("isColliderSticks: ",gameManager.globals.isColliderSticks);
         this.player.checkGravity.apply(this.player); 
         // Para cada morcego, verificar em que sentido ele está indo
         // Se a velocidade for positiva, a escala no eixo X será 1, caso
@@ -278,51 +237,40 @@
         });
     }
         
-    Level2State.prototype.playerBulletCollision = function(ratos, bullet) { //Bala do player colidindo com o inimigos "RATO"
+    Level1State.prototype.playerBulletCollision = function(ratos, bullet) { //Bala do player colidindo com o inimigos "RATO"
         bullet.kill();
         ratos.kill();
         this.player.increaseScoreRatos.apply(this.player);
     }
 
-    Level2State.prototype.diamondCollect = function(player, diamond){ //Jogando Colidindo com o Diamante e indo para o Level 2
+    Level1State.prototype.diamondCollect = function(player, diamond){ //Jogando Colidindo com o Diamante e indo para o Level 2
         diamond.kill();
-        gameManager.globals.environmentSoundLevel2.stop();
-        gameManager.globals.isLevel2 = false
-        gameManager.globals.isLevel3 = true;
+        gameManager.globals.environmentSoundLevel1.stop();
+        gameManager.globals.isLevel1 = false
+        gameManager.globals.isLevel2 = true;
         this.game.state.start('transicao');  
     } 
     
-    Level2State.prototype.fireBulletCollideWall = function(fireBullet){
+    Level1State.prototype.fireBulletCollideWall = function(fireBullet){
         fireBullet.kill();
     }
     
-    Level2State.prototype.fireBullet = function(player, fireBullet){
+    Level1State.prototype.fireBullet = function(player, fireBullet){
         fireBullet.kill();
         this.player.decreaseLives.apply(this.player); 
     }
     
-    Level2State.prototype.sticksCollision = function(player, stick){
-       if (gameManager.globals.isColliderSticks){
-           this.player.decreaseLives.apply(this.player);
-       }
-    }
-    
-    Level2State.prototype.platformsCollision = function(player, platform){
-       platform.body.gravity.y = 50;
-    }
-    
-    Level2State.prototype.ratosCollision = function(player, rato){
+    Level1State.prototype.ratosCollision = function(player, rato){
        if (gameManager.globals.isColliderRatos){
             rato.kill();
             this.player.decreaseLives.apply(this.player);
        }
     }
     
-    Level2State.prototype.fireDeath = function(player, fire){
+    Level1State.prototype.fireDeath = function(player, fire){
         this.player.decreaseLives.apply(this.player);
     }
 
-    gameManager.addState('level2', Level2State);
-
+    gameManager.addState('level1', Level1State);
 
 })();
